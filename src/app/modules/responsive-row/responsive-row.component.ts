c2cd861ae25a87b76e1e7ce2b33fa2613d24bbfb
Ludovic Plantin <ludovic.plantin@moonshotslabs.com>
2019-07-27 20:20:21 +0200

    Align form at the center of the page

--- src/app/modules/responsive-row/responsive-row.component.ts
+++ src/app/modules/responsive-row/responsive-row.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'responsive-row',
   template: '<ng-content></ng-content>',
-  styles: [':host { display: flex;flex-direction: row; flex-wrap: wrap; width: 100%; }']
+  styles: [':host { display: flex; flex-direction: row; flex-wrap: wrap; width: 100%; }']
 })
 export class ResponsiveRowComponent {
 
